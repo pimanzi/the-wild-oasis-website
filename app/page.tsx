9c82dfa520843ec029bad2c530b4a830a83e82ae
pimanzi <p.imanzi@alustudent.com>
2024-10-31 21:08:22 +0200

    creating layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from './components/Navigation';
-
 export default function Page() {
   return (
     <div>
-      <Navigation></Navigation>
       <h1>Hello world</h1>
     </div>
   );
